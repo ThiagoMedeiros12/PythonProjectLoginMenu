6907814203da61945e0abb18e842c730367f5d4d
ThiagoMedeiros12 <thia.med@hotmail.com>
2025-04-08 17:58:43 -0300

    configuração do rest_framework não está funcionando no momento, fiz algumas coisas pra testar

--- jaimeLogin/jaime/settings.py
+++ jaimeLogin/jaime/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Login',
+    'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
