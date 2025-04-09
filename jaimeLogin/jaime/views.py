7bc9d8a52ba64c704dbc157aa22a52c9625ffa6a
ThiagoMedeiros12 <thia.med@hotmail.com>
2025-04-09 10:09:27 -0300

    Login e signup configurado dando 200 ok

--- jaimeLogin/jaime/views.py
+++ jaimeLogin/jaime/views.py
@@ -1,26 +1,40 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 from django.contrib.auth.models import User
 from .serializer import UserSerializer
+from rest_framework.permissions import AllowAny
+from django.shortcuts import get_object_or_404
+
+
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def login(request):
-    return Response ({})
+
+    user = get_object_or_404(User,username=request.data['username'])
+    if not user.check_password(request.data['password']):
+
+        return Response({"detail":"Not found."}, status = status.HTTP_404_NOT_FOUND)
+    
+    token , created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+
+    return Response({"token":token.key,"user": serializer.data})
+
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def signup(request):
-    serialize = UserSerializer(data=request.data)
-    if serialize.is_valid():
-        serialize.save()
+    
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
         user = User.objects.get(username=request.data['username'])
         user.set_password(request.data['password'])
         user.save()
         token = Token.objects.create(user=user)
-        return Response({"token":token.key,"user": serialize.data})
-    return Response (serialize.errors,status = status.HTTP_400_BAD_REQUEST)
+        return Response({"token":token.key,"user": serializer.data})
+    return Response (serializer.errors,status = status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
-def test_token(request):
-    return Response ({})
