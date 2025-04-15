609d44c09e9b639856aa60f4355451d38982bf96
ThiagoMedeiros12 <thia.med@hotmail.com>
2025-04-14 21:52:26 -0300

    configurei para buscar por e-mail,precisa limpar o banco de dados dos testes que fiz, até o momento tudo funcionando de acordo com o esperado, buscando por e-mail e logando por e-mail e senha

--- jaimeLogin/jaime/serializer.py
+++ jaimeLogin/jaime/serializer.py
@@ -2,21 +2,26 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Profile
 
+
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['lastseen']
+
 
 class UserSerializer(serializers.ModelSerializer):
     profile = ProfileSerializer(read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'profile']
+        fields = ['id', 'username', 'password', 'profile','email']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User(username=validated_data['username'])
+        user = User(username=validated_data['username'], email=validated_data['email'])
+        
         user.set_password(validated_data['password'])
         user.save()
         return user
+    
+    
--- jaimeLogin/jaime/views.py
+++ jaimeLogin/jaime/views.py
@@ -12,7 +12,7 @@ from .models import Profile
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def login(request):
-    user = get_object_or_404(User, username=request.data['username'])
+    user = get_object_or_404(User, email=request.data.get('email'))
     if not user.check_password(request.data['password']):
         return Response({"detail": "Invalid credentials."}, status=status.HTTP_404_NOT_FOUND)
 
@@ -27,8 +27,11 @@ def login(request):
     return Response({
         "token": token.key,
         "user": serializer.data,
-        "lastseen": profile.lastseen
+        "lastseen": profile.lastseen,
+        "email": user.email
     }, status=status.HTTP_200_OK)
+
+
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -37,8 +40,7 @@ def signup(request):
     if serializer.is_valid():
         serializer.save()
         user = User.objects.get(username=request.data['username'])
-        user.set_password(request.data['password'])
-        user.save()
+        
 
         token = Token.objects.create(user=user)
 
@@ -49,6 +51,7 @@ def signup(request):
         return Response({
             "token": token.key,
             "user": serializer.data,
+            "email": user.email,
             "lastseen": profile.lastseen
         }, status=status.HTTP_201_CREATED)
     
